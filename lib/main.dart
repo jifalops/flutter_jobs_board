b3358a7b16a555bb22dad946e6a2f305c127eb4e
Jacob Phillips <jifalops@gmail.com>
2019-06-01 14:08:45 -0400

    list view and basic details view

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'firebase.dart';
+
+const appName = 'Flutter Jobs Board';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: appName,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: JobList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class JobList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _JobListState createState() => _JobListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _JobListState extends State<JobList> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(title: Text(appName)),
+      body: FutureBuilder<List<JobPost>>(
+          future: fetchJobs(),
+          builder: (context, snap) {
+            if (snap.hasData) {
+              return ListView(
+                children: snap.data.map((job) => JobPostView(job)).toList(),
+              );
+            } else if (snap.hasError) {
+              return Text('${snap.error}');
+            } else
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+          }),
+    );
+  }
+}
+
+Future<List<JobPost>> fetchJobs() async => (await fetchCollection('jobs'))
+    .documents
+    .map((snap) => JobPost(snap))
+    .toList();
+
+class JobPost {
+  JobPost(DocumentSnapshot snap)
+      : id = snap.documentID,
+        title = snap.data['title'],
+        company = snap.data['company'],
+        location = snap.data['location'],
+        description = snap.data['description'],
+        created = DateTime.fromMillisecondsSinceEpoch(snap.data['created']),
+        iconUrl = snap.data['imageUrl'];
+  final String id, title, company, location, description, iconUrl;
+  final DateTime created;
+}
+
+class JobPostView extends StatelessWidget {
+  JobPostView(this.job);
+  final JobPost job;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => JobDetailsView(job))),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Image.network(job.iconUrl, height: 56, width: 56),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(job.title),
+                  Text(job.location),
+                  Text(job.description,
+                      maxLines: 1, overflow: TextOverflow.clip),
+                ],
+              ))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class JobDetailsView extends StatelessWidget {
+  JobDetailsView(this.job);
+  final JobPost job;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Job Details'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 24),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 28),
+                  child: Image.network(job.iconUrl, height: 64, width: 64),
+                ),
+                Text(job.company, style: Theme.of(context).textTheme.title)
+              ],
             ),
+            SizedBox(height: 24),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              job.title,
+              style: Theme.of(context).textTheme.headline,
+              textAlign: TextAlign.center,
             ),
+            SizedBox(height: 24),
+
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(job.location, style: Theme.of(context).textTheme.subtitle),
+                Text(DateFormat('MMM d').format(job.created)),
+              ],
+            ),
+            SizedBox(height: 24),
+            Text(job.description),
+            Text(job.id),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
